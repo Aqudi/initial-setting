c84234e00af8e20a3b895dab6209814269a3a81b
Aqudi <gjdigj145@gmail.com>
2021-11-17 19:47:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Initial setting
+
+Default settings for personal use
+
+## Font
+
+- [MesloLGS NF Regular.ttf](https://github.com/romkatv/powerlevel10k/blob/master/font.md)
+
+## WSL
+
+- Ubuntu 20.04
+- zsh
+- oh my zsh
+- starship
+
+### Usage
+
+```
+./config_wsl.sh
+```
+
+### Shortcuts
+
+```
+zrc # nano ~/.zshrc
+src # source ~/.zshrc
+```
+
---
+++ config_wsl.sh
@@ -0,0 +1,28 @@
+# package update
+sudo apt update
+
+# install required programs
+sudo apt install zsh curl nano
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# add shortcuts
+echo 'alias zrc="nano ~/.zshrc"' >>~/.zshrc
+echo 'alias src="source ~/.zshrc"' >>~/.zshrc
+
+# disable color highlighting
+echo 'LS_COLORS=$LS_COLORS:"ow=1;34:" ; export LS_COLORS' >>~/.zshrc
+
+# disable zsh theme to use startship
+sed -i 's/ZSH_THEME/#\ ZSH_THEME/' ~/.zshrc
+
+# install starship prompt
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+echo 'eval "$(starship init zsh)"' >>~/.zshrc
+
+# apply startship configuration
+cp starship.toml ~/.config/starship.toml
+
+# apply all changes
+source ~/.zshrc
